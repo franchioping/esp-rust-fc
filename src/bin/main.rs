d0d4fbace48db6e80a862f8b920ccdaefeaf4987
franchioping <franchioping@galard.uk>
2026-06-07 18:37:26 +0100

    working multicore

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,19 +1,43 @@
 #![no_std]
 #![no_main]
 
+use esp_println as _;
+
 use esp_backtrace as _;
-use esp_hal::{main, time::Instant};
+use esp_hal::{
+    main,
+    system::{CpuControl, Stack},
+    time::Instant,
+};
 
 esp_bootloader_esp_idf::esp_app_desc!();
 
+use core::ptr::addr_of_mut;
+
+use defmt::println;
+
 #[main]
 fn main() -> ! {
-    let _peripherals = esp_hal::init(esp_hal::Config::default());
+    let peripherals = esp_hal::init(esp_hal::Config::default());
 
-    defmt::println!("Init!");
+    println!("Init!");
+
+    let mut cpu_control = CpuControl::new(peripherals.CPU_CTRL);
+    static mut APP_CORE_STACK: Stack<{ 1024 * 16 }> = Stack::new();
+    let _guard = cpu_control
+        .start_app_core(unsafe { &mut *addr_of_mut!(APP_CORE_STACK) }, move || {
+            println!("second core started");
+            loop {
+                println!("Bing on 2nd core!");
+
+                let now = Instant::now();
+                while now.elapsed().as_millis() < 5000 {}
+            }
+        })
+        .unwrap();
 
     loop {
-        esp_println::println!("Bing!");
+        println!("Bing!");
 
         let now = Instant::now();
         while now.elapsed().as_millis() < 5000 {}

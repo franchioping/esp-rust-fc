4177b297bc980a964114c19cb70e78ef3746f508
franchioping <franchioping@galard.uk>
2026-06-10 16:26:24 +0100

    remove graphics for now

--- flight-sim/src/main.rs
+++ flight-sim/src/main.rs
@@ -8,26 +8,23 @@ use crate::{input::InputRecording, logger::MsgPackSimLogger};
 pub mod drone;
 pub mod input;
 pub mod logger;
-mod renderer;
 pub mod sim;
 pub mod world;
-
-use blue_engine::{prelude::Engine, primitive_shapes::cube, ObjectSettings};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut world = world::World::new(1.0 / 6000.0);
 
-    // let mut inp_recording: InputRecording = Default::default();
-    // for n in 0..10 {
-    //     inp_recording.add_input(
-    //         n as f32 * 0.2,
-    //         flight_control::controller::Input {
-    //             mode: flight_control::controller::InputMode::ACRO,
-    //             inp: na::vector![n as f32, n as f32 * 2.0, n as f32 / 2.0, 1.0],
-    //         },
-    //     );
-    // }
-    // inp_recording.save_to_file("./run-data/rec.json")?;
+    let mut inp_recording: InputRecording = Default::default();
+    for n in 0..10 {
+        inp_recording.add_input(
+            n as f32 * 0.2,
+            flight_control::controller::Input {
+                mode: flight_control::controller::InputMode::ACRO,
+                inp: na::vector![n as f32, n as f32 * 2.0, n as f32 / 2.0, 1.0],
+            },
+        );
+    }
+    inp_recording.save_to_file("./run-data/rec.json")?;
 
     let drone = drone::Drone::new(
         &mut world,
@@ -56,71 +53,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         InputRecording::load_from_file("./run-data/rec.json")?,
     );
 
-    let mut engine = Engine::new()?;
-    let mut physics_renderer = renderer::PhysicsRenderer::new();
-
-    physics_renderer.build_scene(&sim.world, &mut engine)?;
-
-    cube(
-        "ground",
-        ObjectSettings {
-            shader_settings: blue_engine::ShaderSettings {
-                cull_mode: None,
-                ..Default::default()
-            },
-
-            ..Default::default()
-        },
-        &mut engine.renderer,
-        &mut engine.objects,
-    )?;
-
-    engine
-        .objects
-        .get_mut("ground")
-        .unwrap()
-        .set_scale((20.0, 20.0, 1.0))
-        .set_color(0.5, 0.5, 1.0, 1.0)
-        .set_texture(
-            "background",
-            blue_engine::TextureData::Path("assets/texture_08.png".to_string()),
-            blue_engine::TextureMode::Repeat,
-            &mut engine.renderer,
-        )?;
-
-    engine.update_loop(move |engine| {
-        match sim.step().unwrap() {
-            sim::StepOutcome::Continue => {}
-            sim::StepOutcome::Exit => {
-                sim.shutdown().unwrap();
-            }
-        }
-
-        physics_renderer.sync(&sim.world, engine);
-
-        let drone_pos = sim
-            .world
-            .bodies
-            .get(sim.drone.rb_handle)
-            .unwrap()
-            .position();
-
-        engine.camera.set_position(drone_pos.translation.vector);
-
-        engine.camera.set_target(
-            drone_pos.translation.vector
-                + drone_pos
-                    .rotation
-                    .transform_vector(&na::vector![0.0, 1.0, 0.0]),
-        );
-
-        engine.camera.set_up(
-            drone_pos
-                .rotation
-                .transform_vector(&na::vector![0.0, 0.0, 1.0]),
-        );
-        // engine.camera.set_up(pos);
-    })?;
+    sim.run()?;
 
     Ok(())
 }
